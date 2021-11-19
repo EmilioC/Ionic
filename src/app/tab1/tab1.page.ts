6983c91144c5d10c5bdcf429a93e5f2643809a3a
Emilio Córdoba <emicorgul@gmail.com>
2021-11-19 21:08:19 +0100

    Test with debugger

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
 
+  ngOnInit(){
+    console.log("--OnINIT - TAB1PAGE");
+    debugger;
+    console.log("--1--");
+
+
+    console.log("--2--");
+    debugger;
+    console.log("--3--");
+
+    console.log("--3---");
+  }
 }
