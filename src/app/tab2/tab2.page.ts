09c9c745b509d53a9b22b4e06289c21e1effefb7
EmilioC <emicorgul@gmail.com>
2021-11-20 18:47:03 +0100

    Test with list

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+  name1: string;
+  name2: string;
+  name3: string;
+}
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
+  componentes: Componente[] = [{
+    icon: 'boat-outline',
+    name: 'Carmen',
+    redirecTo: '/tabs/tab3',
+    name1:'Cosas maravillosas',
+    name2:'Cosas maravillosas',
+    name3:'Cosas maravillosas',
+  }];
 
   constructor() {}
 
