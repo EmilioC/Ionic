cc3ad6d78b5415e886e118004d2b5fbf64ce07c0
EmilioC <emicorgul@gmail.com>
2021-11-20 23:19:51 +0100

    Test with alert

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -9,13 +9,17 @@ import { ActionSheetController } from '@ionic/angular';
 })
 export class Tab2Page {
 
-  constructor( private actionSheetCtrl: ActionSheetController) {}
+  constructor( private actionSheetCtrl: ActionSheetController,
+               private alertController: AlertController) {}
 
   onClick(){
 
     console.log('--TAB 2--')
     this.presentActionSheet();
+  }
 
+  alert(){
+    this.presentAlert();
   }
 
   async presentActionSheet() {
@@ -59,7 +63,33 @@ export class Tab2Page {
       }]
     });
     await actionSheet.present();
-
-
 }
+
+    //Alert Button
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        //Obligamos a elegir opción 
+        backdropDismiss: false,
+        subHeader: 'Subtitle',
+        message: 'This is an alert message.',
+        //Originalmente los botones no tienen opciones
+        // buttons: ['OK','Fantastic', 'Impressive']
+        //Pasamos objetos para dar funcionalidad al botón
+        buttons:[{
+          text:'Ok',
+          handler:() =>{
+            console.log('--CLICK -> OK --')
+          }
+        }
+          ,
+          {
+            text:'No - Ok',
+            handler:() =>{
+              console.log('--CLICK -> No OK --')
+            }
+        }]
+      });
+      await alert.present();
+    }
 }
