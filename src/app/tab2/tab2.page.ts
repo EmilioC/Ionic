e6f762a3f1f000c5d905d2d02fb655335a8a0cf1
EmilioC <emicorgul@gmail.com>
2021-11-21 00:00:47 +0100

    Test with alert models

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -22,6 +22,14 @@ export class Tab2Page {
     this.presentAlert();
   }
 
+  alertPrompt(){
+    this.presentAlertPrompt();
+  }
+
+  alertCheckBox(){
+    this.presentAlertCheckbox();
+  }
+
   async presentActionSheet() {
     const actionSheet = await this.actionSheetCtrl.create({
       header: 'Albums',
@@ -76,15 +84,17 @@ export class Tab2Page {
         //Originalmente los botones no tienen opciones
         // buttons: ['OK','Fantastic', 'Impressive']
         //Pasamos objetos para dar funcionalidad al botón
-        buttons:[{
+        buttons:[
+          {
           text:'Ok',
           handler:() =>{
             console.log('--CLICK -> OK --')
           }
-        }
-          ,
+        },
           {
             text:'No - Ok',
+            //Añadimos opción cancelar ya que backdropDimiss: false
+            role:'cancel',
             handler:() =>{
               console.log('--CLICK -> No OK --')
             }
@@ -92,4 +102,181 @@ export class Tab2Page {
       });
       await alert.present();
     }
+
+//Present Alert Promp
+
+async presentAlertPrompt() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Prompt!',
+    inputs: [
+      {
+        name: 'name1',
+        type: 'text',
+        placeholder: 'Placeholder 1'
+      },
+      {
+        name: 'name2',
+        type: 'text',
+        id: 'name2-id',
+        value: 'hello',
+        placeholder: 'Placeholder 2'
+      },
+      // multiline input.
+      {
+        name: 'paragraph',
+        id: 'paragraph',
+        type: 'textarea',
+        placeholder: 'Placeholder 3'
+      },
+      {
+        name: 'name3',
+        value: 'http://ionicframework.com',
+        type: 'url',
+        placeholder: 'Favorite site ever'
+      },
+      // input date with min & max
+      {
+        name: 'name4',
+        type: 'date',
+        min: '2017-03-01',
+        max: '2018-01-12'
+      },
+      // input date without min nor max
+      {
+        name: 'name5',
+        type: 'date'
+      },
+      {
+        name: 'name6',
+        type: 'number',
+        min: -5,
+        max: 10
+      },
+      {
+        name: 'name7',
+        type: 'number'
+      },
+      {
+        name: 'name8',
+        type: 'password',
+        placeholder: 'Advanced Attributes',
+        cssClass: 'specialClass',
+        attributes: {
+          maxlength: 4,
+          inputmode: 'decimal'
+        }
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel');
+        }
+      }, {
+        text: 'Ok',
+        //Recoger datos formulario
+        handler: (data:any) => {
+          console.log(data);
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
+//Alert Check Box
+
+async presentAlertCheckbox() {
+  const alert = await this.alertController.create({
+    cssClass: 'my-custom-class',
+    header: 'Checkbox',
+    inputs: [
+      {
+        name: 'checkbox1',
+        type: 'checkbox',
+        label: 'Checkbox 1',
+        value: 'value1',
+        handler: () => {
+          console.log('Checkbox 1 selected');
+        },
+        checked: true
+      },
+
+      {
+        name: 'checkbox2',
+        type: 'checkbox',
+        label: 'Checkbox 2',
+        value: 'value2',
+        handler: () => {
+          console.log('Checkbox 2 selected');
+        }
+      },
+
+      {
+        name: 'checkbox3',
+        type: 'checkbox',
+        label: 'Checkbox 3',
+        value: 'value3',
+        handler: () => {
+          console.log('Checkbox 3 selected');
+        }
+      },
+
+      {
+        name: 'checkbox4',
+        type: 'checkbox',
+        label: 'Checkbox 4',
+        value: 'value4',
+        handler: () => {
+          console.log('Checkbox 4 selected');
+        }
+      },
+
+      {
+        name: 'checkbox5',
+        type: 'checkbox',
+        label: 'Checkbox 5',
+        value: 'value5',
+        handler: () => {
+          console.log('Checkbox 5 selected');
+        }
+      },
+
+      {
+        name: 'checkbox6',
+        type: 'checkbox',
+        label: 'Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6',
+        value: 'value6',
+        handler: () => {
+          console.log('Checkbox 6 selected');
+        }
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (data:any) => {
+          console.log('--Buttons ALERT CHECK BOX--');
+          console.log(data);
+        }
+      }, {
+        text: 'Ok',
+        handler: (data:any) => {
+          console.log('--TEXT ALERT CHECK BOX--');
+          console.log(data);
+        }
+      }
+    ]
+  });
+
+  await alert.present();
+}
+
 }
