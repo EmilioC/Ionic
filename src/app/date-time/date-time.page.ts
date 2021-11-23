009bd8d98fcdab3725a2fdc08c80d17b18a89014
EmilioC <emicorgul@gmail.com>
2021-11-23 23:05:21 +0100

    Test with customPickerOptions

--- src/app/date-time/date-time.page.ts
+++ src/app/date-time/date-time.page.ts
@@ -8,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class DateTimePage implements OnInit {
 
     fechaNaci: Date = new Date();
+customYearValues = [3012, 3013, 3014];
+
+    customPickerOptions = {
+      buttons: [
+        { 
+          text: "Hello ",
+          handler: ( event) =>{
+            console.log(event);
+          }
+        },
+          { 
+            text: "People ",
+            handler: ( event) =>{
+              console.log(event.day.value);
+            }
+        }
+      ]
+    }
 
   constructor() { }
 
   ngOnInit() {
+  }
+  cambioFecha(nuevaFecha){
+    console.log(new Date (nuevaFecha.detail.value));
+    console.log("--FECHA CAMBIADA--");
   }
 
 }
