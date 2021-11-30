90069e157aa487798f58691d9cebe5b8e2af999f
EmilioC <emicorgul@gmail.com>
2021-11-30 23:58:00 +0100

    Create children routes in the routing module of tabs

--- src/app/tab-s/tab-s-routing.module.ts
+++ src/app/tab-s/tab-s-routing.module.ts
@@ -6,7 +6,21 @@ import { TabSPage } from './tab-s.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabSPage
+    component: TabSPage,
+    children: [
+      {
+        path: 'account',
+        loadChildren: () => import('../avatar/avatar.module').then( m => m.AvatarPageModule )
+      },
+      {
+        path: 'contact',
+        loadChildren: () => import('../list/list.module').then( m => m.ListPageModule )
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('../infinite/infinite.module').then( m => m.InfinitePageModule )
+      }
+    ] 
   }
 ];
 
